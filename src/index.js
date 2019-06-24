e3b0d65c68687b3061f7281a224c167acb460732
Amir Mustafa <amirengg15@gmail.com>
2019-06-24 15:11:49 +0530

    basic react experiments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom"; // For importing react virtual DOM in real DOM we use this
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var element = <h1>Hello World</h1>; // JS X expression
+// console.log(element);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// This root id is the <div id="root"></div> in index.html page
+ReactDOM.render(element, document.getElementById("root")); // rendering virtual DOM to render real DOM
